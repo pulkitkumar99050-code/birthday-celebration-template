06782eac74b50b4f4f682732d8710e72912c8255
pulkitkumar99050-code <pulkitkumar99050@gmail.com>
2025-12-19 16:45:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -155,7 +155,7 @@ function App() {
         </button>
         <MessageCard isActive={currentPage === 3} />
         <button className="page-nav-btn" onClick={() => goToPage(4)}>
-          📸 View Our Memories
+          📸 Some Sweet Memories.. 
         </button>
       </div>
 
